bf8574a9fb7a4dcc326bf7850d309b7ac03ad9ee
daphne <d.van.ginneken@hotmail.com>
2019-01-10 13:32:51 +0100

    probeersel primertest

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def GC(g_seq):
     nucleotide_teller = 0
     for nucl in g_seq:
         nucleotide_teller += 1
-        if nucl == "G" or nucl == "C":
+        if nucl == "g" or nucl == "c":
             gc_teller += 1
     content = gc_teller / nucleotide_teller
     return [content, gc_teller, nucleotide_teller]
@@ -12,44 +12,37 @@ def Temperatuur(gc, at):
     tm = (2*at)+(4*gc)
     return tm
 
-def Test(links, rechts):
-    gc_perc_l = GC(links)[0]
-    gc_teller_l = GC(links)[1]
-    nucleotide_teller_l= GC(links)[2]
-    if (gc_perc_l >= 0.5) and (gc_perc_l <= 0.6):
-        tm_l = Temperatuur(gc_teller_l, (nucleotide_teller_l-gc_teller_l))
-        gc_perc_r = GC(rechts)[0]
-        gc_teller_r = GC(rechts)[1]
-        nucleotide_teller_r = GC(rechts)[2]
-        print(gc_perc_r)
-        if (gc_perc_r >= 0.5) and (gc_perc_r <= 0.6):
-            tm_r = Temperatuur(gc_teller_r, (nucleotide_teller_r - gc_teller_r))
-            print(tm_r)
+def Test(primer):
+    gc_perc = GC(primer)[0]
+    gc_teller = GC(primer)[1]
+    nucleotide_teller= GC(primer)[2]
+    if (gc_perc >= 0.5) and (gc_perc <= 0.6):
+        tm = Temperatuur(gc_teller, (nucleotide_teller-gc_teller))
+        if (tm >= 55) and (tm <= 65):
+            return tm
+        else:
+            return 0
+    else:
+        return 0
 
 def main():
-    #file = open("sequence.fasta", "r")
-    #sequentie = file.read()
-    sequentie = "CCCACGCGTCCGTTCTCTTCCAAGGAACTTTTTGTTTGTTTTATAAAGCAATGGGAAATCCAGAAAACAT\
-    CGAAGATGCTTACGTTGCAGTTATTCGTCCAAAGAACACTGCTAGTCTCAACTCCCGGGAGTATAGAGCT\
-    AAGTCCTATGAAATTTTATTGCATGAAGTTCCCATTGAAGGACAGAAAAAAAAGCGAAAGAAAGTTTTGC\
-    TGGAAACTAAACTTCAAAGCAACAGTGAAATAGCACAAGGCATATTGGACTATGTAGTAGAAACTACCAA\
-    ACCAATTTCTCCTGCAAACCAGGGGATTAAAGGGAAACGAGTGGTGCTGATGAGGAAGTTTCCTCTGGAC\
-    GGAGAGAAGACAGGCAGAGAAGCAGCACTGTTTATCGTGCCATCAGTTGTCAAAGATAATACTAAATATG\
-    CATATACTCCTGGATGCCCAATTTTTTACTGCTTACAAGATATTATGAGAGTTTGTAGTGAATCCAGTAC\
-    TCACTTTGCAACACTTACAGCAAGGATGTTAATAGCCTTGGATAAGTGGTTAGATGAACGTCATGCGCAG\
-    TCTCACTTTATTCCAGCTTTATTCCGACCTTCTCCCCTTGAACGGATAAAGACAAATGTCATAAACCCTG\
-    CGTATGCTGCTGAATTAGGCCAGGTAGACAATTCACTACATATGGGCTATAGTGCACTAGAAATAAAGAG\
-    TAAAATGCTAGCCCTAGAGAAAGCAGACACCTGCATTTACAACCCTTTGTTTGGATCAGATCTTCAGTAT\
-    ACAAATCGGGTAGATAAAGTGGTAATAAATCCATACTTTGGTCTCGGAGCTCCAGACTACTCAAAAATCC\
-    AAATTCCCAAACAGGAAAAATGGCAGCGAAGCATGAGCAGCGTTGTGGAAGACAAAGAACGACAGTGGGT\
-    TGATGACTTTCCTTTACATCGAAATGCCTGTGAAGGAGATTCAGAATTACTGAGCCATCTTCTCGATAAA\
-    GGACTTTCAGTCAACCAACTAGATAATGACCACTGGGCACCCATTCATTATGCATGCTGAAAGCATCTGT\
-    ACCATTGCTTGTCTTCTGAGTATTCTGTTCTGATCTTTTGATTACTGTGTATATGCTGCTGTTCGGAACT"
+    sequentie = "gaattcgaggacgcggaatttgctgtacgcaatgcctttcgcgacgatctgtggggaggggagtctctgcggcgaagcaggcccgtgggaacctcgaccgatccgtagcgggttcgacaagaccaagaccgtcccacgccagcccaaaaagaccccggtcacgaagagctgaccgctcgtttgcggctggtgacagcgttcttaccggcacctctggaccgcgtggcgggggcatcctgacgggggaaggcctgggggaggcagcggtggacttcgcgttcgtgtgcggctggtgcggggaggaatgcgtcgtgtggggcgagcctgtcgtctcttggtggacggacaagtaccgggtgcccgacgacttcgagtgggtcatgtggaggcgtcagcacctccccgcctccgctggacgcccgccgactaggcactgtccggcggatcatgtgaccttctgactggtcgctcgttggttcggtcatgggtcggggggatctgacgaatcgcgagtggtcgttgcttgagccgcatctgccacctttgggtggccggggcggccggtggaacgaccaccgcaccgtggtcaacgggatcctgttccgggtccggaccggtgttctggcgtgatctgccggaacgctatggctcgtggaagaccatctacgagcggcatcgccgctggtcggcggacggcacctgggatcggatcctgcagtcggtccaggccgacgccgacctcgccgggcggatcgactggcgatggtcggcgtcgactcgacgtcctgccgggcccatcagcacgcggccggcgcccgcaagacccggccgcgggtcccgaaaaaaggacaacgccccggcaccaccgccccgacgaggactcggacgtcccggggcggcctgacctgcaagatccacctcgccggcgaaggcggctgccgcccactggccctcctgctcacccccggccaatggggcgatgccccgcaactggtcgggtcctggaccggatcagagtcccccggccgttgggcgggcgaccccggacccggcccgaccacgtcagcggcgacaaggcatacagctcccgccgcaaccgccgctacctgcgaagacccgcatccggcacacgatcccggaaccgaaggaccagcgggccaaccgccgccgcagaggcagagaaggcggcaggcccgccggcttcgaccgggaccactaccggcgacgcaacgaggtcgagcgcaccatcaaccggctcaagaacttccgcgccgtggccacccgttacgacaagagggcctacgtcttcca"
     grens_l = 30
     grens_r = 50
     max_lengte = 30
 
-    links = sequentie[(int(grens_l)-25):int(grens_l)]
-    rechts = sequentie[grens_r:(grens_r+25)]
-    Test(links, rechts)
+
+
+    for lengte in range(17,26):
+        links = sequentie[(int(grens_l)-lengte):int(grens_l)]
+        print("l: ", links)
+        tm_links = Test(links)
+        if tm_links != 0:
+            print(tm_links)
+            for lengte2 in range(17,26):
+                rechts = sequentie[grens_r:(grens_r + lengte)]
+                print("r: ", rechts)
+                tm_rechts = Test(rechts)
+                if tm_rechts != 0:
+                    print(tm_rechts)
 main()
