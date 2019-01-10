7dde2d81919bc27225664ae065798208edd648fd
daphne <d.van.ginneken@hotmail.com>
2019-01-10 14:25:52 +0100

    testen van verschillende primers

--- test.py
+++ test.py
@@ -26,23 +26,35 @@ def Test(primer):
         return 0
 
 def main():
+    """"
+    sequentie = een stuk dna van de notebook
+    grens_l = linkergrens van wat je wilt dupliceren
+    grens_r = rechtergrens van wat je wilt dupliceren
+
+    Eerst op zoek naar een linkerprimer door te kijken naar de mogelijke lengtes tussen 17 en 25.
+    Primers worden in de functie Test gegooid die aan de hand van de functies GC en Temperatuur checken of deze aan de voorwaardes voldoen,
+    zo ja wordt de smelttemperatuur (tm) teruggegeven, zo nee wordt 0 teruggegeven.
+    Als er een goede linkerprimer is gevonden gaat hij hetzelfde doen voor de rechterprimer.
+    Als er 2 goede primers worden gevonden wordt gekeken of de tm dicht genoeg bij elkaar ligt.
+
+
+    """
+
     sequentie = "gaattcgaggacgcggaatttgctgtacgcaatgcctttcgcgacgatctgtggggaggggagtctctgcggcgaagcaggcccgtgggaacctcgaccgatccgtagcgggttcgacaagaccaagaccgtcccacgccagcccaaaaagaccccggtcacgaagagctgaccgctcgtttgcggctggtgacagcgttcttaccggcacctctggaccgcgtggcgggggcatcctgacgggggaaggcctgggggaggcagcggtggacttcgcgttcgtgtgcggctggtgcggggaggaatgcgtcgtgtggggcgagcctgtcgtctcttggtggacggacaagtaccgggtgcccgacgacttcgagtgggtcatgtggaggcgtcagcacctccccgcctccgctggacgcccgccgactaggcactgtccggcggatcatgtgaccttctgactggtcgctcgttggttcggtcatgggtcggggggatctgacgaatcgcgagtggtcgttgcttgagccgcatctgccacctttgggtggccggggcggccggtggaacgaccaccgcaccgtggtcaacgggatcctgttccgggtccggaccggtgttctggcgtgatctgccggaacgctatggctcgtggaagaccatctacgagcggcatcgccgctggtcggcggacggcacctgggatcggatcctgcagtcggtccaggccgacgccgacctcgccgggcggatcgactggcgatggtcggcgtcgactcgacgtcctgccgggcccatcagcacgcggccggcgcccgcaagacccggccgcgggtcccgaaaaaaggacaacgccccggcaccaccgccccgacgaggactcggacgtcccggggcggcctgacctgcaagatccacctcgccggcgaaggcggctgccgcccactggccctcctgctcacccccggccaatggggcgatgccccgcaactggtcgggtcctggaccggatcagagtcccccggccgttgggcgggcgaccccggacccggcccgaccacgtcagcggcgacaaggcatacagctcccgccgcaaccgccgctacctgcgaagacccgcatccggcacacgatcccggaaccgaaggaccagcgggccaaccgccgccgcagaggcagagaaggcggcaggcccgccggcttcgaccgggaccactaccggcgacgcaacgaggtcgagcgcaccatcaaccggctcaagaacttccgcgccgtggccacccgttacgacaagagggcctacgtcttcca"
-    grens_l = 30
-    grens_r = 50
-    max_lengte = 30
-
-
+    grens_l = 130
+    grens_r = 330
 
     for lengte in range(17,26):
         links = sequentie[(int(grens_l)-lengte):int(grens_l)]
-        print("l: ", links)
+        pos_l = int(grens_l)-lengte
         tm_links = Test(links)
         if tm_links != 0:
-            print(tm_links)
             for lengte2 in range(17,26):
-                rechts = sequentie[grens_r:(grens_r + lengte)]
-                print("r: ", rechts)
+                rechts = sequentie[grens_r:(grens_r + lengte2)]
                 tm_rechts = Test(rechts)
                 if tm_rechts != 0:
-                    print(tm_rechts)
+                    verschil = tm_links - tm_rechts
+                    if (verschil >= -2) and (verschil <= 2):
+                        print("links:", pos_l, links, tm_links)
+                        print("rechts:", rechts, tm_rechts)
 main()
